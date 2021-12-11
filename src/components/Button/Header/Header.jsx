70699d9edac0dc51c98accaf72870e407c80c197
ramonruizdev <ramonruizrc@gmail.com>
2021-12-10 21:30:14 -0600

    feat: Component Header

---
+++ src/components/Button/Header/Header.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
+
+const Header = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Box
+                        component="img"
+                        sx={{
+                            height: '100%',
+                            width: 150,
+                        }}
+                        alt="Logo"
+                        src="https://firebasestorage.googleapis.com/v0/b/platzi-master-cms.appspot.com/o/Logo%20Plataforma%201.png?alt=media&token=49037ca8-9eb8-4ea7-80f1-a1aa14f58b50"
+                    />
+                    <Avatar sx={{ bgcolor: deepPurple[500] }}>J</Avatar>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Header;
