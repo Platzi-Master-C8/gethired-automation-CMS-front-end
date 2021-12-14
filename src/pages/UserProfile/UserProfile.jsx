10e6a4f21720bd3a4b1382fa843a8fd8f17ddf80
Dan Ramirez <39846658+danrmrz@users.noreply.github.com>
2021-12-14 17:12:50 -0600

    build: user pages in user profile

---
+++ src/pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserPages from '../../components/UserPages/UserPages';
+
+const serProfile = () => {
+    return (
+        <div className="pagesContainer">
+            <UserPages />
+        </div>
+    );
+};
+
+export default serProfile;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import NotFound from '../pages/NotFound/NotFound';
+import UserProfile from '../pages/UserProfile/UserProfile';
 
 import './style.scss';
 
@@ -9,7 +10,7 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/profile" element={<h1>User Profile</h1>} />
+                <Route exact path="/profile" element={<UserProfile />} />
                 <Route exact path="/edit" element={<h1>User Profile Edit</h1>} />
                 <Route exact path="/webmaker" element={<h1>Web Maker</h1>} />
                 <Route exact path="/preview" element={<h1>Preview</h1>} />
