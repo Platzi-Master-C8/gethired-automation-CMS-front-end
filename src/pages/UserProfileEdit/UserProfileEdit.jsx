204649457e76c615f12a9fe7d8469f8a1cf77471
Dan Ramirez <danrmrz@outlook.com>
2022-02-08 17:09:29 -0600

    Revert "Responsive"

--- src/pages/UserProfileEdit/UserProfileEdit.jsx
+++ src/pages/UserProfileEdit/UserProfileEdit.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Header from '../../components/Header/Header';
 import UserEditForm from '../../components/UserEditForm/UserEditForm';
@@ -13,40 +11,13 @@ const UserProfileEdit = () => {
     return (
         <React.Fragment>
             <Header />
-            <Container fixed>
-                <Box sx={{ margin: '20px 0' }}>
-                    <Box>
-                        <Grid container spacing={2}>
-                            <Grid
-                                item
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
-                                xs={12}
-                                sm={12}
-                                md={4}
-                            >
-                                <UserEditAvatar />
-                            </Grid>
-                            <Grid item xs={12} sm={12} md={8}>
-                                <UserEditForm />
-                            </Grid>
-                        </Grid>
-                    </Box>
-
-                    <Divider
-                        variant="middle"
-                        sx={{ borderColor: '#ae4eff', borderWidth: '1px', width: '85%', margin: '20px auto' }}
-                    />
-
-                    <Box sx={{ m: 2 }}>
-                        <UserEditInfo />
-                    </Box>
-                </Box>
-                <UserEditButtons />
-            </Container>
+            <Stack direction="row" justifyContent="center">
+                <UserEditAvatar />
+                <UserEditForm />
+            </Stack>
+            <Divider sx={{ borderColor: '#ae4eff', borderWidth: '1px', width: '85%', margin: '20px auto' }} />
+            <UserEditInfo />
+            <UserEditButtons />
         </React.Fragment>
     );
 };
